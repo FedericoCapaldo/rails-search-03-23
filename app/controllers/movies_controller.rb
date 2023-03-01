4c3371244f1977a78f5b8f4b27067c90f54a8d69
Federico <federico.capaldo@hotmail.com>
2023-03-01 14:29:02 +0000

    added search bar in index view and simple exact match query in controller

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,7 +1,11 @@
 class MoviesController < ApplicationController
 
   def index
-    @movies = Movie.all
+    if params[:query].present?
+      @movies = Movie.where(title: params[:query])
+    else
+      @movies = Movie.all
+    end
   end
 
 end
